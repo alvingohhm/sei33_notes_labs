d50266c32fbe163b9291ee8ee8b061d356f626d4
alvingohhm <alvgoh@yahoo.com>
2021-12-08 23:41:41 +0800

    Unit2 Day8 update notes1, fix useParams error

--- u2/d8/notes1/src/pages/Details.js
+++ u2/d8/notes1/src/pages/Details.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useParams } from "react-router-dom";
 
 const Details = () => {
-  const params = useParams;
+  const params = useParams();
   return (
     <div>
       <h1>Details of {params.item}</h1>
