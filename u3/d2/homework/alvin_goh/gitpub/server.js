13844bd2f0b803c1a683a45b4c9dc2b17244f3a0
alvingohhm <alvgoh@yahoo.com>
2022-01-06 01:16:23 +0800

    Unit3 Day2 homework ejs completed

---
+++ u3/d2/homework/alvin_goh/gitpub/server.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const drinks = require("./models/drinks");
+const food = require("./models/food");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Gitpub App!");
+});
+
+app.get("/drinks", (req, res) => {
+  res.render("index.ejs", { drinks, food, title: "gitPub - Drinks & Food" });
+});
+
+app.get("/drinks/:id", (req, res) => {
+  let drink = drinks[req.params.id];
+  //capitalize drink name
+  drink.capitalizedName =
+    drink.name.charAt(0).toUpperCase() + drink.name.slice(1);
+  //change image extension
+  drink.image = drink.image.replace("jpg", "png");
+  //format price
+  drink.price = Number(drink.price).toFixed(2);
+  res.render("show.ejs", {
+    item: drink,
+    title: `gitPub - ${drink.capitalizedName}`,
+  });
+});
+
+app.get("/food/:id", (req, res) => {
+  let foodItem = food[req.params.id];
+  //capitalize drink name
+  foodItem.capitalizedName =
+    foodItem.name.charAt(0).toUpperCase() + foodItem.name.slice(1);
+  //change image extension
+  foodItem.image = foodItem.image.replace("jpg", "png");
+  //format price
+  foodItem.price = Number(foodItem.price).toFixed(2);
+  res.render("show.ejs", {
+    item: foodItem,
+    title: `gitPub - ${foodItem.capitalizedName}`,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`
+  🚀  Server is running
+  🔉  Listening on port ${PORT}
+`);
+});
