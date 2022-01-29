caae7f0193967464e1301719f5ed59865ee2993b
alvingohhm <alvgoh@yahoo.com>
2022-01-29 14:35:56 +0800

    modify Unit4 Day6 and Day7 notes

--- u4/d6/notes/django/one_project/one_project/settings.py
+++ u4/d6/notes/django/one_project/one_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-hqzr4b(0gf76x0h^w_q=ceacoq7&y6baaq=g#82-mlfmk69kzc"
+SECRET_KEY = "homework"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
